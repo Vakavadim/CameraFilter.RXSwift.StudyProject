ed5bf216196783fe0b149ea0f69ff19a96302247
Vadim Gamzaev <Vadosbox@gmail.com>
2023-01-20 14:49:06 +0300

    Subscribing and Displaying the selected photo

--- CameraFilter.RXSwift.StudyProject/ViewController.swift
+++ CameraFilter.RXSwift.StudyProject/ViewController.swift
@@ -10,13 +10,25 @@ import RxSwift
 
 class ViewController: UIViewController {
 
-
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Something")
-        
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navC = segue.destination as? UINavigationController,
+              let photosCVC = navC.viewControllers.first as? PhotosCollectionViewController
+        else {
+            fatalError("Segue destination is not found")
+        }
+        
+        photosCVC.selectedPhoto.subscribe { [weak self] photo in
+            self?.photoImageView.image = photo
+        }.disposed(by: disposeBag)
     }
 }
 
